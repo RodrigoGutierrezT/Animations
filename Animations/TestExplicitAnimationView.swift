4e5aa18ff6a895a6de3244e83825775ed3e3c1fa
RodrigoGutierrezT <rodrigo644@gmail.com>
2024-07-11 22:31:42 -0400

    feat: Add animation stack test

--- Animations/TestExplicitAnimationView.swift
+++ Animations/TestExplicitAnimationView.swift
@@ -9,8 +9,10 @@ import SwiftUI
 
 struct TestExplicitAnimationView: View {
     @State var animationAmount = 0.0
+    @State private var enabled = false
     
     var body: some View {
+        Spacer()
         
         Button("Tap Me!") {
             withAnimation(.spring(duration: 0.8, bounce: 0.3)) {
@@ -22,6 +24,21 @@ struct TestExplicitAnimationView: View {
         .foregroundStyle(.white)
         .clipShape(.circle)
         .rotation3DEffect(.degrees(animationAmount),axis: (x: 0, y: 0, z: 1))
+        
+        Spacer()
+        
+        // animation Stack
+        Button("Tap Me") {
+            enabled.toggle()
+        }
+        .frame(width: 200, height: 200)
+        .background(enabled ? .blue : .red)
+        .foregroundStyle(.white)
+        .animation(nil, value: enabled)
+        .clipShape(.rect(cornerRadius: enabled ? 60 : 0))
+        .animation(.spring(duration: 1, bounce: 0.9), value: enabled)
+        
+        Spacer()
         
     }
 }
